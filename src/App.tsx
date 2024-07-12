313cda7b4f14ff2d5b042c88fa2da3156a2b85ba
Arda Yildirim <ardayildirim05@icloud.com>
2024-07-12 21:45:16 +0200

    Implement playback control

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,32 @@ import {
 } from "react-icons/md";
 import { IoSettingsSharp } from "react-icons/io5";
 import { TbRewindForward10, TbRewindBackward10 } from "react-icons/tb";
+import { useRef, useState } from "react";
 
 const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const playVideo = () => {
+    videoRef.current?.play();
+    setIsPlaying(true);
+  };
+
+  const pauseVideo = () => {
+    videoRef.current?.pause();
+    setIsPlaying(false);
+  };
+
+  const togglePlayback = () => {
+    const video = videoRef.current;
+
+    if (!video) return;
+
+    video.paused ? playVideo() : pauseVideo();
+  };
+
   return (
-    <div className="video-container">
+    <div className={`video-container ${!isPlaying ? "paused" : ""}`}>
       <header className="video-container__header">
         <div className="video-container__header__header-controls">
           <FaArrowLeft />
@@ -27,8 +49,8 @@ const App = () => {
         <div className="video-container__footer__timeline-container"></div>
         <div className="video-container__footer__video-controls">
           <div className="video-container__footer__video-controls__left">
-            <FaPlay color="white" />
-            {/* <FaPause /> */}
+            <FaPlay className="play" color="white" onClick={togglePlayback} />
+            <FaPause className="pause" onClick={togglePlayback} />
             {/* <MdVolumeMute />
           <MdVolumeDown /> */}
             <MdVolumeUp />
@@ -45,7 +67,11 @@ const App = () => {
           </div>
         </div>
       </footer>
-      <video className="video-container__video" src="src/assets/Video.mp4" />
+      <video
+        ref={videoRef}
+        className="video-container__video"
+        src="src/assets/Video.mp4"
+      />
     </div>
   );
 };
